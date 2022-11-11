8a2cd1d28500896bd763ad8913f4fadae47b3162
slor37 <915677251@qq.com>
2022-11-11 19:35:14 +0800

    [修改]labelpage

--- src/pages/labelpage/index.vue
+++ src/pages/labelpage/index.vue
@@ -1,4 +1,15 @@
 <template>
+<view class="background">
+<image class="bg" src="/static/bg.jpeg"></image>
+</view>
+<view class="headers">
+<view class="back">
+<image class="back" src="/static/back.jpg"></image>
+</view>
+<view class="title">
+<image class="title" src="/static/title.jpg"></image>
+</view>
+</view>
 
 </template>
 
@@ -9,5 +20,25 @@ export default {
 </script>
 
 <style scoped>
+.bg{
+  width: 100%;
+  height: 100%;
+  position:fixed;
+  background-size:100% 100%;
+  /*z-index 较大的元素会覆盖较小的元素在上层进行显示*/
+  z-index: -1;
+}
+.back {
+  height:70rpx;
+  width: 70rpx;
+  margin-top: 20rpx;
+  margin-left: 11rpx;
+}
+.title {
+  height:120rpx;
+  width: 260rpx;
+  margin-top: 20rpx;
+  margin-left: 20rpx;
+}
 
 </style>
