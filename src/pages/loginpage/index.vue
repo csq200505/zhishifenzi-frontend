dcbdc0cc4a290f158bf4f7c280ee6d9931d37640
Jesse616 <1216499790@qq.com>
2022-11-15 12:11:29 +0800

    登录页面+个人资料页面

--- src/pages/loginpage/index.vue
+++ src/pages/loginpage/index.vue
@@ -1,13 +1,137 @@
 <template>
+  <div class="title">欢迎来到知食分子！</div>
+  <!-- <form action=""></form> <form action=""></form> -->
 
+  <div class="explainSen">
+    <p class="exSen1">· 为提供优质服务，知食分子需获取您的公开信息</p>
+    <p class="exSen2">授权微信头像&昵称</p>
+  </div>
+
+  <button type="submit" style="height:50px;width:230px">授权进入知食分子</button>
+
+  <image class="brand" src="/static/logoSquare.jpg"></image>
+  
+  <div class="Agreement">
+    <label class="pick"><checkbox color="#F79709" style="transform: scale(0.6);" checked></checkbox></label>
+    <span class="pickSen">选中代表您已同意</span>
+    <span class="userAgree">《用户服务协议》</span>
+    <span class="privacyAgree">《隐私权政策》</span>
+  </div>
 </template>
-
 <script>
 export default {
-  name: "index"
+name: "index"
 }
 </script>
 
 <style scoped>
+
+  .title{
+    margin-top: 5%;
+    text-align: center;
+    font-size: 60rpx;
+    font-family:FZYaoti;
+    font-weight: 890;
+  }
+
+  .exSen1{
+    position: absolute;
+    margin-top:62%;
+    margin-left:8%;
+    font-weight: 100;
+    color: rgb(0, 0, 0);
+    font-size: 12px;
+    font-family:  Arial, sans-serif ;
+    font-weight: 550;
+  }
+  .exSen2{
+    position: absolute;
+    margin-top:86%;
+    margin-left:33%;
+    font-weight: 100;
+    color: rgb(117, 114, 114);
+    font-size: 12px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif ;
+  }
+
+  /*Button*/
+  button {
+    position: relative;
+    margin-top: 70%;
+    padding: 1.3em 1.3em;
+    font-size: 16px;
+    margin-left: 14%;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 550;
+    color: #000;
+    background-color: rgb(255, 193, 37);
+    border: none;
+    border-radius: 45px;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    outline: none;
+    line-height:40%  /*button中字体居中*/
+}
+
+button:hover {
+  background-color: #F79709;
+  box-shadow: 0px 15px 20px rgba(204, 143, 51, 0.6);
+  color: #fff;
+  transform: translateY(-7px);
+}
+
+button:active {
+  transform: translateY(-1px);
+}
+
+
+/*agreePick*/
+
+.pick{
+  position: absolute;
+  margin-top: 18.8%;
+  margin-left: 5%;
+  font-size: 11px;
+}
+.pickSen{
+  position: fixed;
+  margin-top: 20%;
+  margin-left: 13%;
+  font-size: 11px;
+}
+.userAgree{
+  position:absolute;
+  margin-left:45%;
+  margin-top: 20%;
+  font-size: 11px;
+  color: #F79709;
+}
+.privacyAgree{
+  position:absolute;
+  margin-left:73%;
+  margin-top: 20%;
+  font-size: 11px;
+  color: #F79709;
+}
+
+/*logo*/
+.brand {
+  height: 365rpx;
+  width: 365rpx;
+  border-radius:200px;
+  position: fixed;
+  top: 14%;
+  left: 26%;
+  box-shadow: 1px 4px 10px 1px rgba(247,151,9,0.7);/*阴影*/
+}
+@keyframes roll{/*创建一个动画*/
+				from{ transform: rotate(-15deg);}
+				to{ transform: rotate(15deg);}
+}
+.brand:hover{/*鼠标移上去时,设置样式*/
+  animation: roll 1.8s ;/*调用动画*/
+}	 
 
 </style>
--- src/pages/perinfopage/index.vue
+++ src/pages/perinfopage/index.vue
@@ -1,13 +1,159 @@
 <template>
+<!-- 头像 -->
+<div class="photo">
+  <div class="img">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30"><path fill="none" d="M0 0h24v24H0z"></path><path d="M4.828 21l-.02.02-.021-.02H2.992A.993.993 0 0 1 2 20.007V3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H4.828zM20 15V5H4v14L14 9l6 6zm0 2.828l-6-6L6.828 19H20v-1.172zM8 11a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" fill="rgba(0,0,0,1)"></path></svg>
+  </div>
+  <span class="word">头像</span>
+</div>
 
+<!-- 昵称 -->
+<div class="Name">
+  <span class="name_word">昵称</span>
+  <div class="name_content">
+    <input type="text" placeholder="贴一张芝士面膜">
+  </div>
+</div>
+
+<!-- 性别 -->
+<div class="sex">
+  <span class="sex_word">性别</span>
+</div>
+<label class="male"><checkbox value="sexPick" style="transform: scale(0.7)" color="#F79709" checked>男</checkbox></label>
+<label class="female"><checkbox value="sexPick" style="transform: scale(0.7)" color="#F79709" >女</checkbox></label>
+
+<!-- 身份 -->
+<div class="birthday">
+  <span class="bir_word">身份</span>
+</div>
+<label class="student"><checkbox value="occupPick" style="transform: scale(0.7)" color="#F79709" checked>学生</checkbox></label>
+<label class="staff"><checkbox value="occupPick" style="transform: scale(0.7)" color="#F79709" >教职工</checkbox></label>
+
+
+<!-- 签名 -->
+<div class="sentence">
+  <span class="sen_word">签名</span>
+  <div class="sen_content">
+    <input type="text" placeholder="谁动了我的奶酪？">
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "index"
+name: "index"
 }
 </script>
 
 <style scoped>
+/* 头像 */
+.img {
+  display: flex;
+  margin: 60px auto 10px auto;
+  width: 55px;
+  height: 60px;
+  border: 1px solid #f0f0f0;
+  border-radius: 50%;
+  font-size: 8px;
+  position: absolute;
+  transition: all 0.5s;
+  z-index: 99;
+  background-color: #152f50;
+  margin-top:6%;
+  margin-left: 20%;
+}
 
+
+
+.word {
+  position:absolute;
+  top: 79rpx;
+  left: 33rpx;
+  padding: 10px;
+  font-weight: 250;
+  font-size: 11px;
+}
+
+svg {
+  padding: 0 5px;
+  cursor: pointer;
+}
+
+.photo:hover .img {
+  transform: scale(1.1);
+  border: 1px solid #f0f0f0;
+}
+
+/* 昵称 */
+.name_word{
+  position: absolute;
+  top:220rpx;
+  left:33rpx;
+  padding: 10px;
+  font-weight: 250;
+  font-size: 11px;
+}
+.name_content{
+  position: absolute;
+  top:233rpx;
+  left: 150rpx ;
+  font-size :11px;
+}
+
+/* 性别 */
+.sex_word{
+  position:absolute;
+  top: 330rpx;
+  left: 33rpx;
+  padding: 10px;
+  font-weight: 250;
+  font-size: 11px;
+}
+/* 生日 */
+.bir_word{
+  position:absolute;
+  top: 440rpx;
+  left: 33rpx;
+  padding: 10px;
+  font-weight: 250;
+  font-size: 11px;
+}
+
+/* 签名 */
+.sen_word{
+  position:absolute;
+  top: 545rpx;
+  left: 33rpx;
+  padding: 10px;
+  font-weight: 250;
+  font-size: 11px;
+}
+.sen_content{
+  position: absolute;
+  top:558rpx;
+  left: 150rpx ;
+  font-size :11px;
+}
+
+.male{
+  position: fixed;
+  margin-top: 45.5%;
+  margin-left:17%;
+}
+.female{
+  position: fixed;
+  margin-top: 45.5%;
+  margin-left:40%;
+}
+
+.student{
+  position: fixed;
+  margin-top: 60%;
+  margin-left:16%;
+}
+.staff{
+  position: fixed;
+  margin-top: 60%;
+  margin-left:38.5%;
+}
 </style>
